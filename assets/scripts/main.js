5f312b901c763f313096be67b09a7b9468f38db5
kari <karinaroldan27@gmail.com>
2023-03-07 19:03:13 -0300

    imprimir categorias

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -34,7 +34,31 @@ function crearTarjetas(cards) {
 }
 
 
+/*let imprimirCategorias = */
+let categorias = data.events.map((evento)=> evento.category)
 
+console.log(categorias);
+
+let categoriasNoRepetidas = new Set(categorias)
+console.log(categoriasNoRepetidas)
+let arrayCategorias = [...categoriasNoRepetidas]
+console.log(arrayCategorias);
+
+let contenedorCategorias = document.querySelector(".contenedorCheck")
+
+function imprimirCategorias(arrayCategorias) {
+    let templateCategorias = ''
+    
+    arrayCategorias.forEach(categoria => {
+        templateCategorias += `<input class="form-check-input" type="checkbox" value=${categoria} id="flexCheckChecked" >
+            <label class="form-check-label" for="flexCheckChecked">
+                ${categoria}
+            </label>`
+    });
+contenedorCategorias.innerHTML = templateCategorias
+}
+
+imprimirCategorias(arrayCategorias)
 
 
 
