50e31e9242a24ed0adafe49c1e273b74530dd41e
kari <karinaroldan27@gmail.com>
2023-03-09 10:13:44 -0300

    filtros

--- assets/scripts/upcoming.js
+++ assets/scripts/upcoming.js
@@ -23,3 +23,41 @@ function imprimirCategorias(arrayCategorias) {
 }
 
 imprimirCategorias(arrayCategorias)
+
+//Filtrar tarjetas por fecha
+let fechaActual = data.currentDate
+console.log(fechaActual);
+
+let cards = data.events
+
+const filteredData = cards.filter(evento => evento.date > fechaActual)
+console.log(filteredData);
+
+
+let contenedorTarjetas = document.querySelector("#contenedor-cards")
+
+let tarjetasUpcoming = new Set (filteredData)
+let arrayUpcoming = [...tarjetasUpcoming]
+console.log(arrayUpcoming)
+
+function imprimirTarjetas(arrayUpcoming) {
+    let templateTarjetas = ''
+    arrayUpcoming.forEach(cards => {
+        templateTarjetas += `<div class="card">
+                <img src="${cards.image}" class="card-img-top" alt="img party">
+                <div class="card-body">
+                    <h5 class="card-title">${cards.name}</h5>
+                    <p class="card-text">${cards.description}
+                    </p>
+                    <p class="card-date">${cards.date}
+                    </p>
+                    <div class="card-price"> <p>$${cards.price}</p>
+                    <a href="#" class="btn btn-primary">See more..</a>
+                    </div>
+                </div>
+            </div> 
+            `
+    });
+    contenedorTarjetas.innerHTML = templateTarjetas
+}
+imprimirTarjetas(filteredData)
