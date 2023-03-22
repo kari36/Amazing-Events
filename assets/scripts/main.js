82513357dc3bff72fc5a1149ee8df3ffcdfe0f9e
kari <karinaroldan27@gmail.com>
2023-03-22 13:49:10 -0300

    task entrega

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -60,7 +60,67 @@ contenedorCategorias.innerHTML = templateCategorias
 
 imprimirCategorias(arrayCategorias)
 
-////////////////////
+////////Buscador de eventos
+////Crear una lista con Elementos(cada elemento es un nombre del evento)
+
+let list = document.getElementById("list")
+console.log(list)
+
+let newCards = addList(cards)
+
+let search = document.getElementById("search")
+console.log(search);
+
+
+function addList(arrayCards) {
+
+    list.innerHTML = ""
+
+    
+
+    arrayCards.forEach((card) => {
+        let listElement = document.createElement("li")
+        listElement += `<div class="card">
+                <img src="${card.image}" class="card-img-top" alt="img party">
+                <div class="card-body">
+                    <h5 class="card-title">${card.name}</h5>
+                    <p class="card-text">${card.description}
+                    </p>
+                    <div class="card-price"> <p>$${card.price}</p>
+                    <a href="/details.html?id=${card._id}" class="btn btn-primary">See more..</a>
+                    </div>
+                </div>
+            </div>`
+        
+        listElement.innerHTML = card 
+    
+        list.appendChild(listElement)
+    })
+}
+addList(cards)
+
+////Cuando se produce un Evento(cambia el input introduciendo texto)
+////Capturar el texto ingresado
+
+
+
+
+ 
+
+search.addEventListener("change", () => {
+    let cardsFiltered = cards.filter((card) => card.name.toLowerCase().includes(search.value.toLowerCase()))
+    console.log(cardsFiltered)
+
+
+
+
+    addList(cardsFiltered)
+
+
+
+})
+
+
 
 /*let inputList = document.querySelectorAll(".check")
 
